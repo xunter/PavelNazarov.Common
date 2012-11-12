f41d62898c93db3a4a97e003ed7d7c838c61275b
Pavel Nazarov <xunter@list.ru>
2012-11-12 14:56:01 +0400

    Added the XmlDocumentExtensions class with the GetOrCreateXmlDeclaration extension method

---
+++ PavelNazarov.Common/Xml/XmlDocumentExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace PavelNazarov.Common.Xml
+{
+    public static class XmlDocumentExtensions
+    {
+        /// <summary>
+        /// Gets the XmlDeclaration if it exists, creates a new if not.
+        /// Got from http://stackoverflow.com/questions/9687791/net-xmldocument-encoding-issue
+        /// </summary>
+        /// <param name="xmlDocument"></param>
+        /// <returns></returns>
+        public static XmlDeclaration GetOrCreateXmlDeclaration(this XmlDocument xmlDocument)
+        {
+            XmlDeclaration xmlDeclaration = null;
+            if (xmlDocument.HasChildNodes)
+                xmlDeclaration = xmlDocument.FirstChild as XmlDeclaration;
+
+            if (xmlDeclaration != null)
+                return xmlDeclaration;
+            //Create an XML declaration. 
+            xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", null, null);
+
+            //Add the new node to the document.
+            XmlElement root = xmlDocument.DocumentElement;
+            xmlDocument.InsertBefore(xmlDeclaration, root);
+            return xmlDeclaration;
+        }
+    }
+}
