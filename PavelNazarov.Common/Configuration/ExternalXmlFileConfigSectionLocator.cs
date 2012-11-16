34569c3fbb4520c46d1af82d898e7706cbe30b11
Pavel Nazarov <xunter@list.ru>
2012-11-16 19:30:15 +0400

    Added Saving method for external config section.

--- PavelNazarov.Common/Configuration/ExternalXmlFileConfigSectionLocator.cs
+++ PavelNazarov.Common/Configuration/ExternalXmlFileConfigSectionLocator.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Configuration;
 using System.Xml.XPath;
 using System.Xml;
+using System.IO;
 
 namespace PavelNazarov.Common.Configuration
 {
@@ -47,6 +48,75 @@ namespace PavelNazarov.Common.Configuration
                 xpathNavigator = xpathNavigator.SelectSingleNode(XPath);
             }
             return xpathNavigator.ReadSubtree();
+        }
+
+        protected virtual XmlDocument LoadExternalXmlAsDocument()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(Path);
+            return xmlDoc;
+        }
+
+        protected virtual XmlElement DeserializeElementForDocument(XmlDocument ownerDocument, Stream source)
+        {
+            using (var xmlReader = XmlReader.Create(source))
+            {
+                var element = (XmlElement)ownerDocument.ReadNode(xmlReader);
+                var elementForDoc = (XmlElement)ownerDocument.ImportNode(element, true);
+                return elementForDoc;
+            }
+        }
+
+        protected virtual Stream SerializeTargetSectionIntoTemporaryStream(ConfigurationSection targetSection)
+        {
+            var tempStream = new MemoryStream();
+            using (var xmlWriter = XmlWriter.Create(tempStream, new XmlWriterSettings { OmitXmlDeclaration = true, ConformanceLevel = ConformanceLevel.Fragment }))
+            {
+                string sectionTagName = (string)targetSection.GetType().GetProperty("ElementTagName", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(targetSection, null);
+                xmlWriter.WriteStartElement(sectionTagName);
+                var serializingResult = (bool)targetSection.GetType().GetMethod("SerializeElement", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Invoke(targetSection, new object[] { xmlWriter, false });
+                if (!serializingResult) throw new ExternalXmlFileConfigSectionLocatorException("Unable to serialize target section!");
+                xmlWriter.WriteEndElement();
+            }
+            return tempStream;
+        }
+
+        protected virtual void ReplaceElement(XmlDocument document, XmlElement elementToReplace, string xpath)
+        {
+            var originalElement = (XmlElement)document.SelectSingleNode(xpath);
+            var parentElement = originalElement.ParentNode;
+            parentElement.ReplaceChild(elementToReplace, originalElement);
+        }
+
+        protected virtual Stream GetOuputStreamToSaveExternalXml()
+        {
+            return File.Create(Path);
+        }
+
+        public void SaveTargetSection(ConfigurationSection targetSection)
+        {
+            try
+            {
+                using (var tempStream = SerializeTargetSectionIntoTemporaryStream(targetSection))
+                {                    
+                    tempStream.Seek(0, SeekOrigin.Begin);
+                    var targetDocument = LoadExternalXmlAsDocument();
+                    var targetElement = DeserializeElementForDocument(targetDocument, tempStream);
+                    ReplaceElement(targetDocument, targetElement, XPath);
+                    using (var streamToSaveDoc = GetOuputStreamToSaveExternalXml())
+                    {
+                        targetDocument.Save(streamToSaveDoc);
+                    }
+                }
+            }
+            catch (ExternalXmlFileConfigSectionLocatorException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new ExternalXmlFileConfigSectionLocatorException("A fatal error has occurred while saving a target section!", ex);
+            }
         }
 
         /// <summary>
---
+++ PavelNazarov.Common/Configuration/ExternalXmlFileConfigSectionLocatorException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PavelNazarov.Common.Configuration
+{
+    
+    [Serializable]
+    public class ExternalXmlFileConfigSectionLocatorException : ApplicationException
+    {
+        public ExternalXmlFileConfigSectionLocatorException()
+            : base()
+        { }
+
+        public ExternalXmlFileConfigSectionLocatorException(string message)
+            : base(message)
+        { }
+
+        public ExternalXmlFileConfigSectionLocatorException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+
+        public ExternalXmlFileConfigSectionLocatorException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        { }
+    }						
+}
