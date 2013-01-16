81878f7e19355bae81f31ef4e076ffc3892e76a0
Pavel Nazarov <xunter@list.ru>
2013-01-16 11:26:05 +0400

    Added the PavelNazarov.Common.ServiceModel.ServiceClientSafeDisposingWrapper class.

---
+++ PavelNazarov.Common/ServiceModel/ServiceClientSafeDisposingWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace PavelNazarov.Common.ServiceModel
+{
+    /// <summary>
+    /// This class wraps a service client to prevent an exception on disposing in Faulted state
+    /// </summary>
+    /// <typeparam name="T">Any service client type that implements the ICommunicationObject interface</typeparam>
+    public class ServiceClientSafeDisposingWrapper<T> : IDisposable where T : ICommunicationObject
+    {
+        private readonly T _serviceClient;
+
+        /// <summary>
+        /// Wrapped service client
+        /// </summary>
+        public T ServiceClient
+        {
+            get { return _serviceClient; }
+        }
+
+        public ServiceClientSafeDisposingWrapper(T serviceClient)
+        {
+            if (serviceClient == null) throw new ArgumentNullException("serviceClient");
+            _serviceClient = serviceClient;
+        }
+
+        public void Dispose()
+        {
+            if (_serviceClient.State == CommunicationState.Faulted)
+            {
+                _serviceClient.Abort();
+            }
+            else if (_serviceClient.State != CommunicationState.Closed)
+            {
+                _serviceClient.Close();
+            }
+        }
+    }
+}
