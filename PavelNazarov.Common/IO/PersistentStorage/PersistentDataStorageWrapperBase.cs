58c1ee8a65b7200b7df30d10eb2e129802b1cf81
Pavel Nazarov <xunter@list.ru>
2013-02-15 17:07:59 +0400

    Added the PavelNazarov.Common.IO.PersistentStorage.PersistentDataStorageWrapperBase class to wrap data such xml data to store it in an object.

---
+++ PavelNazarov.Common/IO/PersistentStorage/PersistentDataStorageWrapperBase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PavelNazarov.Common.IO.PersistentStorage
+{
+    public abstract class PersistentDataStorageWrapperBase<T>
+    {
+        private DateTime _persistentFileLastModifiedDate;
+        private T _storedData;
+        private readonly Object _locker = new Object();
+
+        protected abstract string FilePath { get; }
+
+        protected T StoredData
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(FilePath))
+                {
+                    throw new NullReferenceException("You must specify file path!");
+                }
+                else if (!File.Exists(FilePath))
+                {
+                    throw new FileNotFoundException("File does not found!", FilePath);
+                }
+                var lastModifiedDate = File.GetLastWriteTime(FilePath);
+                lock (_locker)
+                {
+                    bool fileHasChanged = IsPersistentStorageModified();
+                    if (_storedData == null || fileHasChanged)
+                    {
+                        LoadDataFromPersistentStorage(FilePath, lastModifiedDate);
+                    }
+                    return _storedData;
+                }
+            }
+        }
+
+        protected virtual bool IsPersistentStorageModified()
+        {
+            var lastModifiedDate = File.GetLastWriteTime(FilePath);
+            bool fileHasChanged = _persistentFileLastModifiedDate == DateTime.MinValue || _persistentFileLastModifiedDate < lastModifiedDate;
+            return fileHasChanged;
+        }
+
+        private void LoadDataFromPersistentStorage(string filePath, DateTime lastModifiedDate)
+        {
+            _storedData = CustomLoadDataFromPersistentStorage(filePath);
+            _persistentFileLastModifiedDate = lastModifiedDate;
+        }
+
+        protected abstract T CustomLoadDataFromPersistentStorage(string filePath);
+    }
+}
