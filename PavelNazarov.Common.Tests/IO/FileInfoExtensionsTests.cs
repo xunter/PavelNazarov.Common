4c0fe58b66a891fe2fc82a41cf0d07b78e9ca706
Pavel Nazarov <xunter@list.ru>
2013-03-19 15:17:46 +0400

    The Touch extension method for the System.IO.FileInfo class created.

---
+++ PavelNazarov.Common.Tests/IO/FileInfoExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using PavelNazarov.Common.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace PavelNazarov.Common.Tests.IO
+{
+    [TestClass]
+    public class FileInfoExtensionsTests
+    {
+        [TestMethod]
+        public void Test_FileInfoExtensions_Touch()
+        {
+            string testFilePath = Path.GetTempFileName();
+            var fi = new FileInfo(testFilePath);
+            fi.Delete();
+            Assert.IsFalse(fi.Exists);
+            fi.Touch();
+            fi.Refresh();
+            Assert.IsTrue(fi.Exists);
+            var lmd = fi.LastWriteTime;
+            fi.Touch();
+            Assert.AreNotEqual(lmd, fi.LastWriteTime);
+        }
+    }
+}
--- PavelNazarov.Common/IO/FileInfoExtensions.cs
+++ PavelNazarov.Common/IO/FileInfoExtensions.cs
@@ -14,5 +14,25 @@ namespace PavelNazarov.Common.IO
             string version = fileInfo.LastWriteTime.ToString("yyyyMMddHHmmss");
             return version;
         }
+
+        /// <summary>
+        /// Touches the file. It is command like a unix touch one. If the file doesn't exist it will be created and, otherwise, the last modified and write time will be changed to the current.
+        /// </summary>
+        /// <param name="fi"></param>
+        public static void Touch(this FileInfo fi)
+        {
+            if (!fi.Exists)
+            {
+                var fs = fi.Create();
+                fs.Close();
+            }
+            else
+            {
+                var nowDate = DateTime.Now;
+                fi.LastAccessTime = nowDate;
+                fi.LastWriteTime = nowDate;
+            }
+        }
     }
+
 }
