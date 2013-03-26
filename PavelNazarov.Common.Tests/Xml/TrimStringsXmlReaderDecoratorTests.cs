588a8fafa5a3af2383ac0bbb1f5f321fab3b186b
Pavel Nazarov <xunter@list.ru>
2013-03-26 18:07:31 +0400

    Added the PavelNazarov.Common.Xml.TrimTextXmlReaderDecorator class to trim a text node value during a deserialization.

---
+++ PavelNazarov.Common.Tests/Xml/TrimStringsXmlReaderDecoratorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Xml;
+using PavelNazarov.Common.Xml;
+using System.Xml.Linq;
+
+namespace PavelNazarov.Common.Tests.Xml
+{
+    [TestClass]
+    public class TrimStringsXmlReaderDecoratorTests
+    {
+        [TestMethod]
+        public void Test_TrimTextXmlReaderDecorator_ReadTrimmedString()
+        {
+            string xml = @"<item value='ГосП. за выдачу ФЛ справки, подт-й выдачу ВУ или ВР на управ. ТС'>
+ГосП. за выдачу ФЛ справки, подт-й выдачу ВУ или ВР на управ. ТС</item>";
+            var xrs = new XmlReaderSettings() { IgnoreWhitespace = true };
+            using (var reader = new TrimTextXmlReaderDecorator(XmlReader.Create(new StringReader(xml), xrs)))
+            {
+                string expectedVal = "ГосП. за выдачу ФЛ справки, подт-й выдачу ВУ или ВР на управ. ТС";
+                var xitem = XElement.Load(reader);
+                string actual = xitem.Value;
+                Assert.AreEqual(expectedVal, actual);
+            }
+        }
+    }
+}
---
+++ PavelNazarov.Common/Xml/TrimStringsXmlReaderDecorator.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace PavelNazarov.Common.Xml
+{
+    /// <summary>
+    /// Decorates an instance of the XmlReader class and parses text node without any whitespaces. It uses the System.String.Trim method.
+    /// </summary>
+    public class TrimTextXmlReaderDecorator : XmlReader
+    {
+        private readonly XmlReader _original;
+
+        #region XmlReader implementations
+
+        public TrimTextXmlReaderDecorator(XmlReader original)
+        {
+            _original = original;
+        }
+
+        public override int AttributeCount
+        {
+            get { return _original.AttributeCount; }
+        }
+
+        public override string BaseURI
+        {
+            get { return _original.BaseURI; }
+        }
+
+        public override void Close()
+        {
+            _original.Close();
+        }
+
+        public override int Depth
+        {
+            get { return _original.Depth; }
+        }
+
+        public override bool EOF
+        {
+            get { return _original.EOF; }
+        }
+
+        public override string GetAttribute(int i)
+        {
+            return _original.GetAttribute(i);
+        }
+
+        public override string GetAttribute(string name, string namespaceURI)
+        {
+            return _original.GetAttribute(name, namespaceURI);
+        }
+
+        public override string GetAttribute(string name)
+        {
+            return _original.GetAttribute(name);
+        }
+
+        public override bool IsEmptyElement
+        {
+            get { return _original.IsEmptyElement; }
+        }
+
+        public override string LocalName
+        {
+            get { return _original.LocalName; }
+        }
+
+        public override string LookupNamespace(string prefix)
+        {
+            return _original.LookupNamespace(prefix);
+        }
+
+        public override bool MoveToAttribute(string name, string ns)
+        {
+            return _original.MoveToAttribute(name, ns);
+        }
+
+        public override bool MoveToAttribute(string name)
+        {
+            return _original.MoveToAttribute(name);
+        }
+
+        public override bool MoveToElement()
+        {
+            return _original.MoveToElement();
+        }
+
+        public override bool MoveToFirstAttribute()
+        {
+            return _original.MoveToFirstAttribute();
+        }
+
+        public override bool MoveToNextAttribute()
+        {
+            return _original.MoveToNextAttribute();
+        }
+
+        public override XmlNameTable NameTable
+        {
+            get { return _original.NameTable; }
+        }
+
+        public override string NamespaceURI
+        {
+            get { return _original.NamespaceURI; }
+        }
+
+        public override XmlNodeType NodeType
+        {
+            get { return _original.NodeType; }
+        }
+
+        public override string Prefix
+        {
+            get { return _original.Prefix; }
+        }
+
+        public override bool Read()
+        {
+            return _original.Read();
+        }
+
+        public override bool ReadAttributeValue()
+        {
+            return _original.ReadAttributeValue();
+        }
+
+        public override ReadState ReadState
+        {
+            get { return _original.ReadState; }
+        }
+
+        public override void ResolveEntity()
+        {
+            _original.ResolveEntity();
+        }
+
+        public override string Value
+        {
+            get 
+            {
+                if (_original.NodeType == XmlNodeType.Text)
+                {
+                    string originalValue = _original.Value;
+                    if (originalValue != null)
+                    {
+                        var trimmedValue = originalValue.Trim();
+                        return trimmedValue; 
+                    }
+                }
+                return _original.Value; 
+            }
+        }
+
+        #endregion XmlReader implementations
+    }
+}
