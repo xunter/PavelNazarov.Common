5e5f78e1df0d18412740fdb2809f24a9d29310f7
Pavel Nazarov <xunter@list.ru>
2013-05-22 12:08:57 +0400

    The extension method ToNameValueCollection added to convert a dictionary to an instance of the NameValueCollection class.

---
+++ PavelNazarov.Common.Tests/Collections/DictionaryExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PavelNazarov.Common.Collections;
+
+namespace PavelNazarov.Common.Tests.Collections
+{
+    [TestClass]
+    public class DictionaryExtensionsTests
+    {
+        [TestMethod]
+        public void Test_DictionaryExtensions_ToNameValueCollection()
+        {
+            var dict = new Dictionary<string, string> { { "name", "value" } };
+            var coll = dict.ToNameValueCollection();
+            Assert.AreEqual(dict.Count, coll.Count);
+            foreach (KeyValuePair<string, string> pair in dict)
+            {
+                bool keyExists = false;
+                for (int i = 0; i < coll.Keys.Count; i++)
+                {
+                    if (coll.Keys[i] == pair.Key)
+                    {
+                        keyExists = true;
+                        break;
+                    }
+                }
+                Assert.IsTrue(keyExists);
+                Assert.AreEqual(pair.Value, coll[pair.Key]);
+            }
+        }
+    }
+}
