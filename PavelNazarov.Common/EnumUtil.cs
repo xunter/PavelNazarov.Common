68d3f59e747ebee5d067d8cefbfccbbff5e7e113
Pavel Nazarov <xunter@list.ru>
2013-05-24 11:38:22 +0400

    The class PavelNazarov.Common.EnumUtil added.

---
+++ PavelNazarov.Common/EnumUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PavelNazarov.Common
+{
+    /// <summary>
+    /// Provides some useful functions for enum types.
+    /// </summary>
+    public static class EnumUtil
+    {
+        /// <summary>
+        /// Parses a enum member from the string ignoring case.
+        /// </summary>
+        /// <typeparam name="T">a enum type</typeparam>
+        /// <param name="val">string value</param>
+        /// <returns>a enum member</returns>
+        public static T Parse<T>(string val)
+        {
+            if (String.IsNullOrEmpty(val))
+            {
+                throw new ArgumentNullException("val");
+            }
+            var enumType = typeof(T);
+            if (!enumType.IsEnum)
+            {
+                throw new ArgumentException("The type parameter T must be a Enum type.");
+            }
+
+            foreach (string enumMemberName in Enum.GetNames(enumType))
+            {
+                if (String.Compare(enumMemberName, val, true) == 0)
+                {
+                    return (T)Enum.Parse(enumType, enumMemberName);
+                }
+            }
+
+            return default(T);
+        }
+    }
+}
