189e87c9413620fe3cd4c1ab6e82ac28e6b8285c
xunter <xunter@list.ru>
2013-10-24 16:49:15 +0400

    The CollectionsExtensions class added.

---
+++ PavelNazarov.Common.Tests/Collections/CollectionExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using PavelNazarov.Common.Collections;
+
+namespace PavelNazarov.Common.Tests.Collections
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        class Foo
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+
+            public static Foo Factory
+            {
+                get
+                {
+                    return new Foo { Name = Guid.NewGuid().ToString(), Value = Guid.NewGuid().ToString() };
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Test_CollectionExtensions_SortByPropertyName()
+        {
+            var foos = Enumerable.Range(0, 100).Select(i => Foo.Factory).ToArray();
+
+            var foosSortedByName = foos.SortByPropertyName("Name").ToArray();
+            var foosOrderedByName = foos.OrderBy(f => f.Name).ToArray();
+
+            Assert.AreEqual(foosOrderedByName[50], foosSortedByName[50]);
+            Assert.AreNotEqual(foosOrderedByName[50], foosSortedByName[51]);
+        }
+
+        [TestMethod]
+        public void Test_CollectionExtensions_DoPaging()
+        {
+            var foos = Enumerable.Range(0, 100).Select(i => Foo.Factory).ToArray();
+            var pagedFoos = foos.DoPaging(5, 10);
+            var manualPagedFoos = foos.Skip(40).Take(10);
+            Assert.IsTrue(pagedFoos.Union(manualPagedFoos).Count() == pagedFoos.Count());
+        }
+    }
+}
---
+++ PavelNazarov.Common/Collections/CollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace PavelNazarov.Common.Collections
+{
+    public static class CollectionExtensions
+    {
+        public static IOrderedEnumerable<T> SortByPropertyName<T>(this IEnumerable<T> collection, string propertyName, string dir = "ASC", string defaultSort = null)
+        {
+            Type productType = typeof(T);
+            var propInfo = productType.GetProperty(propertyName);
+            if (propInfo == null && defaultSort != null)
+            {
+                propInfo = productType.GetProperty(defaultSort);
+            }
+            var productParamExpr = Expression.Parameter(productType, "p");
+            var sortingFunc = Expression.Lambda<Func<T, Object>>(Expression.TypeAs(Expression.MakeMemberAccess(productParamExpr, propInfo), typeof(Object)), productParamExpr).Compile();
+            return dir == "ASC" ? collection.OrderBy(sortingFunc) : collection.OrderByDescending(sortingFunc);
+        }
+
+        public static IEnumerable<T> DoPaging<T>(this IEnumerable<T> collection, int page, int rowsForPage)
+        {
+            return collection.Skip(rowsForPage * (page - 1)).Take(rowsForPage);
+        }
+    }
+}
