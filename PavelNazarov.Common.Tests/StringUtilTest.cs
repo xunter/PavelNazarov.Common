21cfa59671deda630223cfb5b65a852785a323fa
Pavel Nazarov <xunter@list.ru>
2013-11-06 14:00:57 +0400

    The StringUtil class added.

---
+++ PavelNazarov.Common.Tests/StringUtilTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PavelNazarov.Common.Tests
+{
+    [TestClass]
+    public class StringUtilTest
+    {
+        [TestMethod]
+        public void Test_StringUtil_ToStringSafe_ShouldReturnStringEmptyWhenNull()
+        {
+            Assert.AreEqual(String.Empty, StringUtil.ToStringSafe(null));
+        }
+
+        [TestMethod]
+        public void Test_StringUtil_ToStringSafe_ShouldReturnString()
+        {
+            DateTime nowDate = DateTime.Now;
+            Assert.AreEqual(nowDate.ToString(), StringUtil.ToStringSafe(nowDate));
+        }
+
+        [TestMethod]
+        public void Test_StringUtil_MaskString()
+        {
+            string cardNumber = "1234567890123456";
+            string maskedCardNumber = "************3456";
+            Assert.AreEqual(maskedCardNumber, StringUtil.MaskString(cardNumber, 0, 12));
+        }
+    }
+}
---
+++ PavelNazarov.Common/StringUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PavelNazarov.Common
+{
+    /// <summary>
+    /// The class contains some usefull miscellaneous functions
+    /// </summary>
+    public static class StringUtil
+    {
+        /// <summary>
+        /// Invokes the System.Object ToString method if it is not null, otherwise, returns String.Empty
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToStringSafe(Object obj)
+        {
+            return obj == null ? String.Empty : obj.ToString();
+        }
+
+        /// <summary>
+        /// Masks a string from startIndex to (startIndex + count) using the specified mask char (default is '*')
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="count"></param>
+        /// <param name="maskChar"></param>
+        /// <returns></returns>
+        public static string MaskString(string str, int startIndex, int count, char maskChar = '*')
+        {
+            var sb = new StringBuilder(str);
+            sb.Remove(startIndex, count);
+            sb.Insert(startIndex, String.Empty.PadRight(count, maskChar));
+            return sb.ToString();
+        }
+    }
+}
